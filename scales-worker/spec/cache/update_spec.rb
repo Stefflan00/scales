9af915ecb3748597a4c268052fa0bc2d16c2cbb9
Thomas Fankhauser <tommylefunk@googlemail.com>
2012-07-21 23:18:18 +0200

    fixed queue name

--- scales-worker/spec/cache/update_spec.rb
+++ scales-worker/spec/cache/update_spec.rb
@@ -5,7 +5,7 @@ describe Scales::Worker::Cache::Update do
   it "adds paths to the threads post_process_jobs queue" do
     in_process_thread do
       described_class.update "/track", "/tracks/1/edit", "/navigation"
-      Thread.current[:post_process_jobs].should == ["/track", "/tracks/1/edit", "/navigation"]
+      Thread.current[:post_process_queue].should == ["/track", "/tracks/1/edit", "/navigation"]
     end
   end
   
@@ -13,28 +13,28 @@ describe Scales::Worker::Cache::Update do
     it "adds update paths to the threads post_process_jobs queue" do
       in_process_thread do
         Scales.update "/track", "/tracks/1/edit", "/navigation"
-        Thread.current[:post_process_jobs].should == ["/track", "/tracks/1/edit", "/navigation"]
+        Thread.current[:post_process_queue].should == ["/track", "/tracks/1/edit", "/navigation"]
       end
     end
     
     it "adds refresh paths to the threads post_process_jobs queue" do
       in_process_thread do
         Scales.refresh "/track", "/tracks/1/edit", "/navigation"
-        Thread.current[:post_process_jobs].should == ["/track", "/tracks/1/edit", "/navigation"]
+        Thread.current[:post_process_queue].should == ["/track", "/tracks/1/edit", "/navigation"]
       end
     end
     
     it "adds reload paths to the threads post_process_jobs queue" do
       in_process_thread do
         Scales.reload "/track", "/tracks/1/edit", "/navigation"
-        Thread.current[:post_process_jobs].should == ["/track", "/tracks/1/edit", "/navigation"]
+        Thread.current[:post_process_queue].should == ["/track", "/tracks/1/edit", "/navigation"]
       end
     end
     
     it "adds repush paths to the threads post_process_jobs queue" do
       in_process_thread do
         Scales.repush "/track", "/tracks/1/edit", "/navigation"
-        Thread.current[:post_process_jobs].should == ["/track", "/tracks/1/edit", "/navigation"]
+        Thread.current[:post_process_queue].should == ["/track", "/tracks/1/edit", "/navigation"]
       end
     end
   end
