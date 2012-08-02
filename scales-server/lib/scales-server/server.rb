491c7896033eabec0fc5403b4abed6753f886fc0
Thomas Fankhauser <tommylefunk@googlemail.com>
2012-08-02 22:03:35 +0200

    added 404 debug logger to server

--- scales-server/lib/scales-server/server.rb
+++ scales-server/lib/scales-server/server.rb
@@ -1,13 +1,34 @@
 module Scales
   module Server
     
+    require 'logger'
+    @@logger = ::Logger.new('log/scales_server.log')
+    
+    class << self
+      def logger
+        @@logger
+      end
+    end
+    
     class Server < Goliath::API
       use Goliath::Rack::Params
       use Goliath::Rack::DefaultMimeType
       use ContentType, 'html'
 
       def response(env)
-        Dispatch.request(env)
+        response = Dispatch.request(env)
+        
+        if response.first == 404
+          if response.last.is_a?(String)
+            Scales::Server.logger.error(response.last)
+          else
+            message = ""
+            response.last.each{ |chunk| message << chunk }
+            Scales::Server.logger.error(message)
+          end
+        end
+        
+        response
       end
 
     end
